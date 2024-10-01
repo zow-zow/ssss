e8a40ecfb73f3696c5649b24b49bf462be3aaf72
zow-zow <146065081+zow-zow@users.noreply.github.com>
2024-10-02 02:53:27 +0800

    Update iptv.py

--- iptv.py
+++ iptv.py
@@ -68,7 +68,7 @@ def fetch_province_data(province):
         "shortcuts": [],
         "query": f'octet-stream AND UDPXY AND province: "{province}"',
         "start": 0,
-        "size": 10,
+        "size": 80,
         "device": {
             "device_type": "PC",
             "os": "Windows",
