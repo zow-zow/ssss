97e313f1ff86f16092a05ea702c07ff681e875a7
zow-zow <146065081+zow-zow@users.noreply.github.com>
2024-10-03 21:48:42 +0800

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,11 @@
+#读取链接内容
+import requests
+import re
+
+
+
+with open('oldlinkstest.txt', 'w', encoding='utf-8') as f:
+    f.write('删除new数据成功\n')
+with open('results.txt', 'w', encoding='utf-8') as f:
+    f.write('删除old数据成功\n')
+
