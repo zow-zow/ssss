3d82a6c740ee2b246ea9dc67ccf24e96934da71c
zow-zow <146065081+zow-zow@users.noreply.github.com>
2024-10-04 15:56:53 +0800

    Update delete.py

--- delete.py
+++ delete.py
@@ -5,7 +5,7 @@ import re
 
 
 with open('oldlinkstest.txt', 'w', encoding='utf-8') as f:
-    f.write('删除new数据成功\n')
+    f.write('删除test数据成功\n')
 with open('results.txt', 'w', encoding='utf-8') as f:
     f.write('删除old数据成功\n')
 
