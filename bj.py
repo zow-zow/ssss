2a33fb58feb36e147aa1dcc53b2d8ca89e9ba78a
zow-zow <146065081+zow-zow@users.noreply.github.com>
2024-10-27 16:58:58 +0800

    Update bj.py

--- bj.py
+++ bj.py
@@ -1,243 +1,209 @@
 import requests
-import re
 import time
 import concurrent.futures
 
+# # 基础的POST请求和Headers
+# url = 'https://quake.360.net/api/search/query_string/quake_service'
+# headers = {
+#     'Host': 'quake.360.net',
+#     'Connection': 'keep-alive',
+#     'Content-Length': '514',
+#     'sec-ch-ua-platform': '"Windows"',
+#     'Authorization': '233',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0',
+#     'Accept': 'application/json, text/plain, */*',
+#     'sec-ch-ua': '"Microsoft Edge";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+#     'Content-Type': 'application/json',
+#     'sec-ch-ua-mobile': '?0',
+#     'Origin': 'https://quake.360.net',
+#     'Sec-Fetch-Site': 'same-origin',
+#     'Sec-Fetch-Mode': 'cors',
+#     'Sec-Fetch-Dest': 'empty',
+#     'Referer': 'https://quake.360.net/quake/',
+#     'Accept-Encoding': 'gzip, deflate, br, zstd',
+#     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+#     'Cookie': '__guid=261090063.613249672348867000.1716045888585.0752; Qs_lvt_347988=1716045889%2C1727758734; Qs_pv_347988=232592375406089440%2C2694674839023988700; Q=u%3DMBJ_1990%26n%3DMBJ_1990%26le%3DZGxkZQD5AwH5WGDjpKRhL29g%26m%3DZGZ2WGWOWGWOWGWOWGWOWGWOZmL0%26qid%3D30236351%26im%3D1_t0179f90761614136d3%26src%3Dpcw_pinpaizhongxin%26t%3D1; __NS_Q=u%3DMBJ_1990%26n%3DMBJ_1990%26le%3DZGxkZQD5AwH5WGDjpKRhL29g%26m%3DZGZ2WGWOWGWOWGWOWGWOWGWOZmL0%26qid%3D30236351%26im%3D1_t0179f90761614136d3%26src%3Dpcw_pinpaizhongxin%26t%3D1; cert_common=e76bf9d9-bc20-4a98-9f03-cfa370f72aa6; Qs_lvt_357693=1722167193%2C1722308122%2C1722398281%2C1722586464%2C1727758821; T=s%3D29492d3a3c509a1c3dcc142fd0e674b1%26t%3D1727758879%26lm%3D2-1%26lf%3D2%26sk%3D35b1f48167d1fa49a753a56e5926741d%26mt%3D1727758879%26rc%3D%26v%3D2.0%26a%3D1; __NS_T=s%3D29492d3a3c509a1c3dcc142fd0e674b1%26t%3D1727758879%26lm%3D2-1%26lf%3D2%26sk%3D35b1f48167d1fa49a753a56e5926741d%26mt%3D1727758879%26rc%3D%26v%3D2.0%26a%3D1; Qs_pv_357693=85128068538245360%2C1354647596654044700%2C4278164041916076000%2C602192517421148700%2C2674543313704729000'
+# }
 
-# 定义初始URL列表
-urls = ['https://fofa.info/result?qbase64=InVkcHh5IiAmJiByZWdpb249ImJlaWppbmci&page=1&page_size=20',
+# 可同时处理的省份列表
+#provinces = ['天津','上海',]
+provinces = ['北京', '天津', '上海', '重庆', '河北', '山西', '内蒙古', '辽宁', 
+            '吉林', '黑龙江', '江苏', '浙江', '安徽', '福建', '江西', '山东', 
+            '河南', '湖北', '湖南', '广东', '广西', '海南', '四川', '贵州',
+                '云南', '西藏', '陕西', '甘肃', '青海', '宁夏', '新疆']
 
+    
 
-
-
-
-    ]  # 添加更多URL
-
-html_contents = []  # 用于存储获取到的HTML源码
-
-# 发送GET请求，获取HTML源代码
-def get_html_content(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    return ''  # 如果请求失败返回空字符串
-
-# 使用并发处理来同时发送多个GET请求
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    # 执行并发的GET请求，并获取HTML源码
-    html_contents = list(executor.map(get_html_content, urls))
-
-# 将获取到的HTML源码组合在一起
-combined_html = ' '.join(html_contents)  # 将所有HTML源码组合在一起
-
-print(combined_html)
-    # 使用正则表达式提取URL
-pattern = r'http://\d+\.\d+\.\d+\.\d+:\d+'
-found_urls = re.findall(pattern, combined_html)
-
-usable_urls = []
-for original_url in found_urls:
-    test_url = original_url + '/status'
+# 从 URL 加载后缀
+def load_suffixes(url):
     try:
-        test_response = requests.get(test_url, timeout=2)
-        if test_response.status_code == 200:
-            print(f"可用URL: {original_url}")
-            usable_urls.append(original_url)
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+        response.raise_for_status()  # 确保请求成功
+        urls = []
+        for line in response.text.splitlines():
+            url = line.strip().split(',')[0]  # 只取逗号前面的部分
+            urls.append(url)
+        return urls
     except requests.exceptions.RequestException as e:
-        print(f"请求失败: {e}")
-    # 使用集合来存储提取到的URL，以确保不重复
-found_urls_set = set()
-for original_url in usable_urls:
-        for suffix in ['/rtp/239.3.1.241:8000','/rtp/239.3.1.159:8000','/rtp/239.3.1.158:8000','/rtp/239.3.1.242:8000','/rtp/239.3.1.116:8000','/rtp/239.3.1.117:8000','/rtp/239.3.1.118:8000','/rtp/239.3.1.115:8000','/rtp/239.3.1.189:8000','/rtp/239.3.1.120:8000','/rtp/239.3.1.121:8000','/rtp/239.3.1.243:8000','/rtp/239.3.1.235:8000','/rtp/239.3.1.236:2000','/rtp/239.3.1.249:8001','/rtp/239.3.1.250:8001','/rtp/239.3.1.111:8001','/rtp/239.3.1.94:4120','/rtp/239.3.1.212:8060','/rtp/239.3.1.58:8156','/rtp/239.3.1.114:8001','/rtp/239.3.1.71:4120','/rtp/239.3.1.95:8001','/rtp/239.3.1.112:8001','/rtp/239.3.1.100:8001','/rtp/239.3.1.113:8001','/rtp/239.3.1.238:8001','/rtp/239.3.1.102:8001','/rtp/239.3.1.163:8001','/rtp/239.3.1.221:8001','/rtp/239.3.1.154:8001','/rtp/239.3.1.187:8001','/rtp/239.3.1.96:8001','/rtp/239.3.1.125:8001','/rtp/239.3.1.126:8001','/rtp/239.3.1.127:8001','/rtp/239.3.1.128:8001','/rtp/239.3.1.75:4120','/rtp/239.3.1.76:4120','/rtp/239.3.1.68:4120','/rtp/239.3.1.80:4120','/rtp/239.3.1.69:4120','/rtp/239.3.1.85:4120','/rtp/239.3.1.147:9268','/rtp/239.3.1.77:4120','/rtp/239.3.1.67:4120','/rtp/239.3.1.83:4120','/rtp/239.3.1.81:4120','/rtp/239.3.1.79:4120','/rtp/239.3.1.90:4120','/rtp/239.3.1.87:4120','/rtp/239.3.1.86:4120','/rtp/239.3.1.73:4120','/rtp/239.3.1.72:4120','/rtp/239.3.1.78:4120',]:
-            new_url = original_url + suffix
-            found_urls_set.add(new_url)
+        print(f"Failed to load suffixes from {url}: {e}")
+        return []
 
-    # 将集合转换为列表，以便后续处理
-urls = list(found_urls_set)
+# 从 URL 加载 URL 对应名称
+def load_url_names(url):
+    try:
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+        response.raise_for_status()  # 确保请求成功
+        formatted_names = {}
+        for line in response.text.splitlines():
+            line = line.strip()  # 去除前后的空白字符
+            # 检查行是否有效，包含逗号并且非空
+            if line and ',' in line:
+                url, name = line.split(',', 1)  # 采用split(',',1)以避免过多分割
+                formatted_names[url.split('/')[-1]] = name  # 仅保留路径部分作为键
+            else:
+                print(f"无效行：{line}")  # 打印无效行以供调试
+        return formatted_names
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to load URL names from {url}: {e}")
+        return {}
+
+# 获取省份数据
+def fetch_province_data(province):
+    headers = {
+        'Host': 'www.zoomeye.org',
+        'Connection': 'keep-alive',
+        'Cube-Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImEwNWRiYmIxNTA1NiIsImVtYWlsIjoiMTkxMDQ5NjU5QHFxLmNvbSIsImV4cCI6MTczMDA1NDcyMC4wfQ.d35Goc_OgCKXKLqZxtuKgXbo7Th52Bl60jlFccGhMuQ',
+        'sec-ch-ua-platform': '"Windows"',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0',
+        'Accept': 'application/json, text/plain, */*',
+        'sec-ch-ua': '"Chromium";v="130", "Microsoft Edge";v="130", "Not?A_Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        #'Referer': 'https://www.zoomeye.org/searchResult?q=app%3A%22udpxy+multicast+UDP-to-HTTP%22++%2Bsubdivisions%3A%22%E5%8C%97%E4%BA%AC%22&page=1&pageSize=50',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+        'Cookie': '__jsluid_s=7d9cc8c3315d6d4c14d336d3105edf57; token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImEwNWRiYmIxNTA1NiIsImVtYWlsIjoiMTkxMDQ5NjU5QHFxLmNvbSIsImV4cCI6MTcyOTk2MDE3Ny4wfQ.pKOL8BGZz2FuKY7SGhdEgRDWQ6oc1SXouZ0_UcDSny8',
+        'If-None-Match': 'W/"be6364d336b5b8f4d112349dd5166a37d1cd7cdc"'
+    }
+    url = f"https://www.zoomeye.org/api/search?q=app%3A%22udpxy+multicast+UDP-to-HTTP%22++%2Bsubdivisions%3A%22{province}%22&page=1&pageSize=50&t=v4%2Bv6%2Bweb"
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    if response.status_code == 200:
+        #print (data)
+        return data  #   # 返回JSON格式的内容
+    
 
 
+# 提取host和port
+def extract_urls(json_data):
+    found_urls = []
+    if 'matches' in json_data:
+        for match in json_data['matches']:
+            ip = match['ip']
+            port = match['portinfo']['port']
+            url = f"http://{ip}:{port}"
+            found_urls.append(url)
+    #print(found_urls)
+    return found_urls
+# 测量下载速度
 def measure_download_speed(url, name, duration=10):
     try:
-        print(f"开始测量下载速度：{url}")
+       # print(f"开始测量下载速度：{url}")
         start_time = time.time()
-        response = requests.get(url, stream=True, timeout=3)  # 设置超时时间
+        response = requests.get(url, stream=True, timeout=3)
 
-        total_downloaded = 0  # total downloaded data in bytes
-        for data in response.iter_content(1024*1024):  # read 1MB at a time
+        total_downloaded = 0
+        for data in response.iter_content(1024 * 1024):
             total_downloaded += len(data)
-            break  # 读取完1MB的数据后立即结束循环
+            break  # 读取完1MB数据后立即结束
 
         elapsed_time = time.time() - start_time
         if elapsed_time > duration:
-            speed = 0.0  # 如果总时间超过10秒，赋值速度为0
+            speed = 0.0  # 超过时限则速度为0
         else:
-            speed = total_downloaded / elapsed_time / 1024 / 1024  # speed in MB/s
-        return name, url, speed  # 返回name和url
-    except (requests.exceptions.RequestException, ConnectionError) as e:
-        print(f"Failed to download {url}: {e}")
-        return url, 0.0
+            speed = total_downloaded / elapsed_time / 1024 / 1024  # MB/s
+            print(f"发现有效源：{name} -- {url} -- {speed:.2f} MB/s")
+        return name, url, speed
+    except requests.exceptions.RequestException as e:
+        # print(f"Failed to download {url}: {e}")
+        return name, url, 0.0
 
+# 为每个省份处理数据
+def process_province(province, output_file):
+    print(f"处理 {province} 数据")
 
-def main():
-    print("开始处理URL和测量下载速度")
-    formatted_names = {}  # 使用字典来存储每个URL对应的名称
-    for original_url in urls:
-        print(f"正在处理URL: {original_url}")
-        if '/rtp/111.11.11.111:111' in original_url:
-            formatted_names[original_url] = "模板"
-        elif '/rtp/239.3.1.241:8000' in original_url:
-            formatted_names[original_url] = "BRTV北京卫视HD"
-        elif '/rtp/239.3.1.159:8000' in original_url:
-            formatted_names[original_url] = "BRTV新闻HD"
-        elif '/rtp/239.3.1.158:8000' in original_url:
-            formatted_names[original_url] = "BRTV影视HD"
-        elif '/rtp/239.3.1.242:8000' in original_url:
-            formatted_names[original_url] = "BRTV文艺HD"
-        elif '/rtp/239.3.1.116:8000' in original_url:
-            formatted_names[original_url] = "BRTV财经HD"
-        elif '/rtp/239.3.1.117:8000' in original_url:
-            formatted_names[original_url] = "BRTV生活HD"
-        elif '/rtp/239.3.1.118:8000' in original_url:
-            formatted_names[original_url] = "BRTV青年HD"
-        elif '/rtp/239.3.1.115:8000' in original_url:
-            formatted_names[original_url] = "BRTV纪实科教HD"
-        elif '/rtp/239.3.1.189:8000' in original_url:
-            formatted_names[original_url] = "BRTV卡酷少儿HD"
-        elif '/rtp/239.3.1.120:8000' in original_url:
-            formatted_names[original_url] = "BRTV冬奥纪实"
-        elif '/rtp/239.3.1.121:8000' in original_url:
-            formatted_names[original_url] = "BRTV冬奥纪实"
-        elif '/rtp/239.3.1.243:8000' in original_url:
-            formatted_names[original_url] = "BRTV体育休闲"
-        elif '/rtp/239.3.1.235:8000' in original_url:
-            formatted_names[original_url] = "BTV国际频道"
-        elif '/rtp/239.3.1.236:2000' in original_url:
-            formatted_names[original_url] = "爱上4K"
-        elif '/rtp/239.3.1.249:8001' in original_url:
-            formatted_names[original_url] = "4K超清"
-        elif '/rtp/239.3.1.250:8001' in original_url:
-            formatted_names[original_url] = "淘电影HD"
-        elif '/rtp/239.3.1.111:8001' in original_url:
-            formatted_names[original_url] = "每日影院HD"
-        elif '/rtp/239.3.1.94:4120' in original_url:
-            formatted_names[original_url] = "星影"
-        elif '/rtp/239.3.1.212:8060' in original_url:
-            formatted_names[original_url] = "纪实人文HD"
-        elif '/rtp/239.3.1.58:8156' in original_url:
-            formatted_names[original_url] = "金鹰纪实HD"
-        elif '/rtp/239.3.1.114:8001' in original_url:
-            formatted_names[original_url] = "风尚生活HD"
-        elif '/rtp/239.3.1.71:4120' in original_url:
-            formatted_names[original_url] = "地理"
-        elif '/rtp/239.3.1.95:8001' in original_url:
-            formatted_names[original_url] = "淘剧场HD"
-        elif '/rtp/239.3.1.112:8001' in original_url:
-            formatted_names[original_url] = "幸福剧场HD"
-        elif '/rtp/239.3.1.100:8001' in original_url:
-            formatted_names[original_url] = "淘娱乐HD"
-        elif '/rtp/239.3.1.113:8001' in original_url:
-            formatted_names[original_url] = "幸福娱乐HD"
-        elif '/rtp/239.3.1.238:8001' in original_url:
-            formatted_names[original_url] = "淘BabyHD"
-        elif '/rtp/239.3.1.102:8001' in original_url:
-            formatted_names[original_url] = "萌宠TVHD"
-        elif '/rtp/239.3.1.163:8001' in original_url:
-            formatted_names[original_url] = "朝阳融媒HD"
-        elif '/rtp/239.3.1.221:8001' in original_url:
-            formatted_names[original_url] = "通州融媒HD"
-        elif '/rtp/239.3.1.154:8001' in original_url:
-            formatted_names[original_url] = "密云电视台HD"
-        elif '/rtp/239.3.1.187:8001' in original_url:
-            formatted_names[original_url] = "延庆电视台"
-        elif '/rtp/239.3.1.96:8001' in original_url:
-            formatted_names[original_url] = "房山电视台"
-        elif '/rtp/239.3.1.125:8001' in original_url:
-            formatted_names[original_url] = "睛彩竞技HD"
-        elif '/rtp/239.3.1.126:8001' in original_url:
-            formatted_names[original_url] = "睛彩篮球HD"
-        elif '/rtp/239.3.1.127:8001' in original_url:
-            formatted_names[original_url] = "睛彩羽毛球HD"
-        elif '/rtp/239.3.1.128:8001' in original_url:
-            formatted_names[original_url] = "睛彩广场舞HD"
-        elif '/rtp/239.3.1.75:4120' in original_url:
-            formatted_names[original_url] = "解密"
-        elif '/rtp/239.3.1.76:4120' in original_url:
-            formatted_names[original_url] = "军事"
-        elif '/rtp/239.3.1.68:4120' in original_url:
-            formatted_names[original_url] = "军旅剧场"
-        elif '/rtp/239.3.1.80:4120' in original_url:
-            formatted_names[original_url] = "动画"
-        elif '/rtp/239.3.1.69:4120' in original_url:
-            formatted_names[original_url] = "古装剧场"
-        elif '/rtp/239.3.1.85:4120' in original_url:
-            formatted_names[original_url] = "台球"
-        elif '/rtp/239.3.1.147:9268' in original_url:
-            formatted_names[original_url] = "嘉佳卡通"
-        elif '/rtp/239.3.1.77:4120' in original_url:
-            formatted_names[original_url] = "国学"
-        elif '/rtp/239.3.1.67:4120' in original_url:
-            formatted_names[original_url] = "城市剧场"
-        elif '/rtp/239.3.1.83:4120' in original_url:
-            formatted_names[original_url] = "墨宝"
-        elif '/rtp/239.3.1.81:4120' in original_url:
-            formatted_names[original_url] = "好学生"
-        elif '/rtp/239.3.1.79:4120' in original_url:
-            formatted_names[original_url] = "早教"
-        elif '/rtp/239.3.1.90:4120' in original_url:
-            formatted_names[original_url] = "武侠剧场"
-        elif '/rtp/239.3.1.87:4120' in original_url:
-            formatted_names[original_url] = "武术"
-        elif '/rtp/239.3.1.86:4120' in original_url:
-            formatted_names[original_url] = "爱生活"
-        elif '/rtp/239.3.1.73:4120' in original_url:
-            formatted_names[original_url] = "美人"
-        elif '/rtp/239.3.1.72:4120' in original_url:
-            formatted_names[original_url] = "美妆"
-        elif '/rtp/239.3.1.78:4120' in original_url:
-            formatted_names[original_url] = "戏曲"
+    # 加载该省份的后缀和URL对应名称
+    suffixes = load_suffixes(f'http://8.138.87.43:2020/%E7%BB%84%E6%92%AD/url_names_{province}.txt')
+    url_names = load_url_names(f'http://8.138.87.43:2020/%E7%BB%84%E6%92%AD/url_names_{province}.txt')
 
+    # 获取该省份的JSON数据
+    json_data = fetch_province_data(province)
+    found_urls = extract_urls(json_data)  # 提取URL
 
+    usable_urls = []
+    for original_url in found_urls:
+        test_url = f"{original_url}/status"
+        try:
+            response = requests.get(test_url, timeout=2)
+            if response.status_code == 200:
+                print(f"发现可用URL：{test_url}")
+                usable_urls.append(original_url)
+        except requests.exceptions.RequestException:
+            pass
 
+    # 构建带后缀的URL
+    urls = set()
+    for original_url in usable_urls:
+        for suffix in suffixes:
+            urls.add(f"{original_url}{suffix}")
 
+    # 并发测量下载速度
+    results = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_url = {executor.submit(measure_download_speed, url, url_names.get(url.split('/')[-1], "Unknown")): url for url in urls}
 
-         
+        for future in concurrent.futures.as_completed(future_to_url):
+            result = future.result()
+            results.append(result)
 
-           
-
-
-
-          
-
-                    
-
-
-
-
-
-
-
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = list(executor.map(lambda url: measure_download_speed(url, formatted_names.get(url, "Unknown")), urls))
-
-    unique_urls = set()  # 存储唯一的URL
-    for result in results:
-        if len(result) >= 3 and result[2] > 0:  # 检查result的长度是否至少为3
-          unique_urls.add(result[0])  # 将结果按相同的 result[0] 进行存储
-
-    # 将结果按相同的 result[0] 进行排序
+    # 将结果按name排序
     sorted_results = sorted(results, key=lambda x: x[0])
 
-    # 在插入内容前添加标签
-    with open("ziyong.txt", "w") as file:
-        file.write("\n#北京,#genre#\n")
+    # 处理完一个省份，立即写入文件
+    with open(output_file, "a", encoding='utf-8') as file:  # 以追加模式打开文件
+        file.write(f"\n#{province},#genre#\n")  # 写入省份名称作为区分
+        for name, url, speed in sorted_results:
+            if speed > 0:
+                file.write(f"{name},{url} -- {speed:.2f} MB/s\n")
+
+def main():
+    output_file = "zoom_results.txt"
+    
+    # 清空文件内容（如果文件存在）
+    open(output_file, "w").close()
+
+    # 顺序处理每个省份的数据
+    # for province in provinces:
+    #     process_province(province, output_file)
+
+    # 并发处理每个省份的数据，但每个请求间隔1秒
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
         
-    # 输出结果到文件 ztv.txt
-    with open("ziyong.txt", "a") as file:
-        for result in sorted_results:
-            if len(result) >= 3 and result[2] > 0:  # 同样在这里检查
-                file.write(f"{result[0]},{result[1]} -- {result[2]:.2f} MB/s\n")
-    print("处理完成，结果已写入 ziyong.txt 文件")
+        futures = []
+        for province in provinces:
+            futures.append(executor.submit(process_province, province, output_file))
+            time.sleep(5)  # 在每个任务提交前延迟1秒
+
+        
+    # 等待所有任务完成
+    concurrent.futures.wait(futures)
+
+    print(f"所有省份数据处理完成，结果写入 {output_file}")
 
 if __name__ == "__main__":
     main()
